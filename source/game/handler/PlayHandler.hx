195495df9a3ab9d6883d6ea8235cdb5b3c3034c5
Eric Lund <github@egotron.com>
2015-08-22 16:46:39 -0400

    Added monster.

--- source/game/handler/PlayHandler.hx
+++ source/game/handler/PlayHandler.hx
@@ -16,33 +16,40 @@ import flaxen.service.InputService;
 
 class PlayHandler extends FlaxenHandler
 {
+	public static var MAX_SPEED:Int = 10;
+	public var monsterSpeed:Int = 0;
+
 	override public function start()
 	{
-		var e = f.newEntity("sky")
+		f.newEntity("sky")
 			.add(new Image("art/sky.png"))
 			.add(Size.screen())
 			.add(new Layer(100))
 			.add(Position.zero());
 
-		var e = f.newEntity("sun")
+		f.newEntity("sun")
 			.add(new Image("art/sun.png"))
 			.add(new Layer(90))
 			.add(new Position(15, 15));
 
-		var e = f.newEntity("clouds")
+		f.newEntity("clouds")
 			.add(new Image("art/clouds.png"))
 			.add(Repeating.instance)
 			.add(new Layer(80))
 			.add(Position.zero());
 
-		var e = f.newEntity("mountains")
+		f.newEntity("mountains")
 			.add(new Image("art/mountains.png"))
 			.add(Repeating.instance)
 			.add(new Layer(70))
 			.add(Position.zero());
 
+		f.newEntity("monster")
+			.add(new Image("art/monster.png"))
+			.add(new Position(10, 193))
+			.add(new Layer(30));
 
-		updateVelocity(5);
+		updateSpeed(0);
 	}
 
 	override public function update()
@@ -60,23 +67,31 @@ class PlayHandler extends FlaxenHandler
 				trace(setName + ":{" + f.getComponentSet(setName) + "}");
 		}
 
-		if(key == Key.DIGIT_0) updateVelocity(0);
-		if(key == Key.DIGIT_1) updateVelocity(1);
-		if(key == Key.DIGIT_2) updateVelocity(2);
-		if(key == Key.DIGIT_3) updateVelocity(3);
-		if(key == Key.DIGIT_4) updateVelocity(4);
-		if(key == Key.DIGIT_5) updateVelocity(5);
+		if(key == Key.DIGIT_0) updateSpeed(0);
+		if(key == Key.DIGIT_1) updateSpeed(1);
+		if(key == Key.DIGIT_2) updateSpeed(2);
+		if(key == Key.DIGIT_3) updateSpeed(3);
+		if(key == Key.DIGIT_4) updateSpeed(4);
+		if(key == Key.DIGIT_5) updateSpeed(5);
 		#end
+
+		if(key == Key.SPACE && monsterSpeed < MAX_SPEED) 
+			updateSpeed(monsterSpeed + 1);
 
 		InputService.clearLastKey();
 	}
 
-	private function updateVelocity(speed:Int)
+	private function updateSpeed(speed:Int)
 	{
 		for(o in [{name:"clouds", vel:5}, {name:"mountains", vel:20}])//, ["items", -50]])
 		{
 			var vel:Float = o.vel + o.vel * speed * 0.5;
 			f.resolveEntity(o.name).add(new Velocity(-vel, 0));
 		}
+
+		var pos = f.resolveComponent("monster", Position);
+		pos.x = 5 + 6 * speed;
+
+		monsterSpeed = speed;
 	}
 }
